1fe04a5e9712c4ea8155edf7fd2b10d70b8bf4ab
Megan ODonovan <megan.odonovan@paddypowerbetfair.com>
2022-03-11 09:17:08 +0000

    Weekly Task 02 Solution

---
+++ Week02/bmi.py
@@ -0,0 +1,23 @@
+# Programming and Scripting
+# Weekly Tasks 02
+# Author: Megan O'Donovan
+
+## Summary : read in values and print calculated bmi value
+
+weight = int(input('Enter weight(kg):'))
+height = int(input('Enter height(cm):'))
+# BMI is measured using kg and meters squared - height measured in cm must be changed 
+newheight = (height/100)**2
+
+# round to 2 decimal places
+bmivalue = round(weight/newheight,2)
+
+#print('The BMI is (kg/m^2) {}.' .format(bmivalue))  // This will print the bmi but the measure units won't be superscripted
+print("The BMI is (kg/m\u00b2)  {}.".format(bmivalue))
+
+#######
+# Second method 
+# Another solution instead of using round(,2) (Line 12) is to declare the number of decimal places and include this in the format()
+# bmivalue = (weight/newheight)
+# decimals = 2
+ #print("The BMI is (kg/m\u00b2)  {0:.{1}f}.".format(bmivalue, decimals))
