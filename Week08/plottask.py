747d134d29cfec806242433714940f5e4a5af439
megan1103 <98594902+megan1103@users.noreply.github.com>
2022-04-06 15:28:00 +0100

    Weekly Task 08 Solution
    Added comments to explain commands

--- Week08/plottask.py
+++ Week08/plottask.py
@@ -8,16 +8,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 # Create coordinates:
-x1 = np.random.randint(0,4,10)
-x2 =x1**2
-x3 =x1 **3
+x1 = np.random.randint(0,4,10) # Choose random integers between 0 and 4 in 10 steps
+x2 =x1**2 # multiple x1 coordinates to the power of 2
+x3 =x1 **3 # multiple x1 coordinates to the power of 3
 
-plt.plot(x1, marker = 'o', linestyle = 'dotted', color = 'green',label = 'f(x)=x')
-plt.plot(x2, marker = 'o', linestyle = 'dashed', color = 'blue',label = 'g(x)=x\u00b2')
-plt.plot(x3, marker = 'o', linestyle = 'solid', color = 'black',label = 'h(x)=x\u00b3')
+plt.plot(x1, marker = 'o', linestyle = 'dotted', color = 'green',label = 'f(x)=x') # marker is used to highlight the points on the graph
+plt.plot(x2, marker = 'o', linestyle = 'dashed', color = 'blue',label = 'g(x)=x\u00b2') # linestyle helps to distinguish between lines
+plt.plot(x3, marker = 'o', linestyle = 'solid', color = 'black',label = 'h(x)=x\u00b3') # in the BMI task I learnt how to use supscripts, same method is used here in the labelling
 
 font = {'family':'serif','color':'darkred','size':15}
 
 plt.title("Weekly Task 8", fontdict = font)
 plt.legend(loc = 'upper left')
-plt.show()
+plt.show() # regardless of lines 15-17 creating the plots, plt.show() is needed to actually show the plot
